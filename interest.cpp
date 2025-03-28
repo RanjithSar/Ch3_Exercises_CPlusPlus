4336e2d6b7a94bd2aa6cb907407ff026b222855c
Ranjith <itssrranjith@gmail.com>
2025-03-27 23:03:54 -0400

    changed initial_amt variable from int to float, initialized total variable to initial amount, made for loop calculating total amount with interest, and printed out interest

--- interest.cpp
+++ interest.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	long initial_amt = 0;
+	float initial_amt = 0.0;
 	int years = 0;
 	float interest_rate = 0.0;
 	cout << "Enter initial amount: ";
@@ -11,4 +11,11 @@ int main(){
 	cin >> years;
 	cout << "Enter interest (percent per year): ";
 	cin >> interest_rate;
+	
+	float total = initial_amt;
+	for (int i = 0; i < years; i++){
+		total += (total * (interest_rate / 100));
+	}
+	cout << "At the end of " << years << " years, you will have " << total << " dollars." << endl;
+	return 0;
 }
