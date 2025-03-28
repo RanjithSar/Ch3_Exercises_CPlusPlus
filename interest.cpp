24a3cc2620e49d931f9e9c02f9cc38f271a0eebb
Ranjith <itssrranjith@gmail.com>
2025-03-27 22:58:36 -0400

    created main method, initialized variables, and asked for input for variables

---
+++ interest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long initial_amt = 0;
+	int years = 0;
+	float interest_rate = 0.0;
+	cout << "Enter initial amount: ";
+	cin >> initial_amt;
+	cout << "Enter number of years: ";
+	cin >> years;
+	cout << "Enter interest (percent per year): ";
+	cin >> interest_rate;
+}
